fae87883320461e5ebeb847420530dff8c899dfb
Daniel Garcia <daes_g@hotmail.com>
2023-03-16 21:48:01 -0500

    envio de correo con python

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_migrate import Migrate
+from dotenv import load_dotenv
+from os import environ
+from db import conexion
+from flask_restful import Api
+
+from utils.enviar_correo import enviar_correo_adjuntos
+from controllers.usuario_controller import RegistroController
+
+load_dotenv()
+
+app = Flask(__name__) 
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
+conexion.init_app(app)
+
+api = Api(app)
+
+Migrate(app, conexion)
+
+@app.route('/prueba')
+def enviar_correo_prueba():
+    # correo al cual va a llegar la información
+    enviar_correo_adjuntos('ederiveroman@gmail.com', 'Correo con imagenes')
+
+    return {
+        'message': 'Correo enviado exitosamente'
+    }
+
+api.add_resource(RegistroController, '/registro')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
