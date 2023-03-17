3f317053810a43107e90e7f40e3528dfdaab406b
Daniel Garcia <daes_g@hotmail.com>
2023-03-16 23:03:02 -0500

    subida de imagenes y devolución de las mismas

--- app.py
+++ app.py
@@ -7,11 +7,14 @@ from flask_restful import Api
 
 from utils.enviar_correo import enviar_correo_adjuntos
 from controllers.usuario_controller import RegistroController
+from controllers.categoria_controller import ImagenesCOntroller
 
 load_dotenv()
 
 app = Flask(__name__) 
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
+app.config['UPLOAD_FOLDER'] = '/imagenes'
+
 conexion.init_app(app)
 
 api = Api(app)
@@ -28,6 +31,7 @@ def enviar_correo_prueba():
     }
 
 api.add_resource(RegistroController, '/registro')
+api.add_resource(ImagenesCOntroller, '/imagenes', '/imagenes/<nombre>')
 
 if __name__ == '__main__':
     app.run(debug=True)
