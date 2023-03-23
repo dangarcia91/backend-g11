375d72eece18762cc54c3febbd11cd8a7906d425
Daniel Garcia <daes_g@hotmail.com>
2023-03-22 19:25:51 -0500

    tarea corregida

--- controllers/producto_controller.py
+++ controllers/producto_controller.py
@@ -1,5 +1,8 @@
 from flask_restful import Resource, request
+from werkzeug.utils import secure_filename
 from os import path
+
+from uuid import uuid4
 from db import conexion
 from models.producto_model import Producto
 from dtos.producto_dto import ProductoDto, MostrarProductoDto
@@ -9,19 +12,33 @@ class ProductosController(Resource):
         data = request.form.to_dict()
         # TODO: validar que tengamos esa llave en el formulario llamada 'imagen'
         # TODO: validar que solo sean imágenes
+        mimetype_valido = 'image/'
         imagen = request.files.get('imagen')
         # TODO: agregar un uuid al nombre de la imagen
         # TODO: no recibir imagenes que pesen más de 10Mb
-        data['imagen'] = imagen.filename
+        #                   B   KB      MB
+        CONTENIDO_MAXIMO = 10 * 1024 * 2024
         try:
+            if image is None:
+                raise Exception ("Se necesita una imagen para poder crear el producto")
+            
+            if mimetype_valido not in imagen.mimetype:
+                raise Exception('El tipo de archivo no es válido')
+            if imagen.content_length > CONTENIDO_MAXIMO:
+                raise Exception('El archivo es muy pesado')
+            
             # 
             dto = ProductoDto()
+            nombre_imagen = secure_filename(uuid4().hex +'_'+ imagen.filename)
+            data['imagen'] = 'imagenes/' + nombre_imagen
             data_serializada = dto.load(data)
+
             nuevo_producto = Producto(**data_serializada)
-
             conexion.session.add(nuevo_producto)
-            imagen.save(path.join('imagenes', data['imagen']))
 
+            #imagen.save(path.join('imagenes', data['imagen']))
+            imagen.save(path.join('imagenes', nombre_imagen))
+            
             conexion.session.commit()
             return {
                 'message': 'Producto creado exitosamente'
